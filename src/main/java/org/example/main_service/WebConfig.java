60d6667ca1197251f510edf89b860c6513c250a6
Adlett Anafiya <adlet.anafiya@mail.ru>
2024-12-06 21:37:55 +0500

    cors config

--- src/main/java/org/example/main_service/WebConfig.java
+++ src/main/java/org/example/main_service/WebConfig.java
@@ -1,16 +1,23 @@
 package org.example.main_service;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.List;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    @Override
-    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-        converters.add(new MappingJackson2HttpMessageConverter());
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:5050")
+                        .allowedMethods("GET")
+                        .allowedHeaders("*");
+            }
+        };
     }
 }
